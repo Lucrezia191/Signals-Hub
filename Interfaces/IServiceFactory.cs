29cfcd2f9da595eb881a2bed49024e8b0f9847ed
Lucrezia191 <lucrezia.fabrizio191@gmail.com>
2021-11-15 22:26:29 +0100

    Interfaces.IServiceFactory

---
+++ Interfaces/IServiceFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signals_hub.Interfaces
+{
+    public interface IServiceFactory
+    {
+        T CreateInjectableService<T>(object[] optionalParams = null);
+    }
+
+    public interface Injectable
+    {
+
+    }
+}
